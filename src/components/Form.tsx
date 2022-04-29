d96de6633940362c54b9537ca1e8e19fd4dafc08
Yuri Vitor <yuri.lins25@gmail.com>
2022-04-29 14:49:22 -0300

    validate only number

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,41 @@
 import { useContext } from 'react';
+import NumberFormat, { NumberFormatValues } from 'react-number-format';
 import { ResinContext } from '../context/ResinContext';
 
 const Form = () => {
   const { setCurrentResin, setExpectedResin } = useContext(ResinContext);
 
-  const handleCurrentResinForm = async (event: {
-    target: HTMLInputElement | EventTarget;
-  }) => {
-    const value = (event.target as HTMLTextAreaElement).value;
-    setCurrentResin(Number(value));
+  const handleResinForm = (event: any, type: string) => {
+    const value = event.target.value;
+
+    if (type === 'current') {
+      setCurrentResin(Number(value));
+    }
+    if (type === 'expected') {
+      setExpectedResin(Number(value));
+    }
   };
 
-  const handleExpectedResinForm = async (event: {
-    target: HTMLInputElement | EventTarget;
-  }) => {
-    const value = (event.target as HTMLTextAreaElement).value;
-    setExpectedResin(Number(value));
+  const maxResinAllowed = (inputValue: NumberFormatValues) => {
+    const { value } = inputValue;
+
+    return Number(value) <= 160;
   };
 
   return (
     <div className="app-resin-form">
-      <input
-        type="text"
+      <NumberFormat
+        isAllowed={maxResinAllowed}
         className="app-resin-form-current"
         placeholder="Type your current resin"
-        onChange={handleCurrentResinForm}
+        onChange={(event: any) => handleResinForm(event, 'current')}
       />
-      <input
-        type="text"
+
+      <NumberFormat
+        isAllowed={maxResinAllowed}
         className="app-resin-form-expected"
         placeholder="What is the expected resin?"
-        onChange={handleExpectedResinForm}
+        onChange={(event: any) => handleResinForm(event, 'expected')}
       />
     </div>
   );
